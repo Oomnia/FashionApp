10ef0e6817da421e30a7c261d7a9de87d7eb730d
Omnia Said <omniasaied13201@gmail.com>
2025-10-12 17:12:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:fashion_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FashionApp());
+}
+
+class FashionApp extends StatelessWidget {
+  const FashionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+       home: SplashScreen());
+  }
+}
